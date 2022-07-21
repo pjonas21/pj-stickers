05dac3c657274a1c4c62a65b5b67a2f1146c7b0e
Paulo Jonas <pjonas21@gmail.com>
2022-07-21 16:51:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## **Projeto Aula 1 - Imersão Java Alura**
+
+### Projeto desenvolvido para aprendizado da linguagem de programação Java, durante a Imersão Java da Alura.
+
+![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge)
+
+![Captura terminal](capturaTerminal.png "Captura terminal")
+
+## 🛠️ Funcionalidades do projeto
+
+- `Funcionalidade 1`: Acessar API de filmes do IMDB
+- `Funcionalidade 2`: Exibir os dados da lista de melhores filmes da platadorma, link para seu poster e sua classificação
+
+## 📁 Acesso ao projeto
+
+- Baixe ou clone o projeto;
+- Abra a pasta src e depois o arquivo "App.java";
+
+
+  `Caso tenha interesse, a URL pode ser alterada para a plataforma de sua preferência, basta abrir a plataforma desejada realizar ocadastro e utilizar sua API de acesso.`
+
+## 🛠️ Abrir e rodar o projeto
+
+- Basta executar o código e gerar sua lista de filmes.
---
+++ src/App.java
@@ -0,0 +1,40 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // acessar url da plataforma de filmes
+        //String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
+        String url = "https://alura-imdb-api.herokuapp.com/movies"; // desafio 1 - alterar a url da API
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // extrair os dados de titulo fime, imagem, classificação
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir os dados recebidos da url
+        for (Map<String,String> filme : listaDeFilmes) {
+            StringBuilder estrela = new StringBuilder("\u001b[97;1m\u001b[44;5;88m");
+            int somaEstrelas;
+
+            for (somaEstrelas = 0; somaEstrelas < Double.valueOf(filme.get("imDbRating")).intValue();somaEstrelas++){
+                estrela.append("\u2B50");
+            }
+
+            System.out.println("\u001b[1mT\u00EDtulo: " + filme.get("title") + "\u001b[m");
+            System.out.println("\u001b[1mPoster: \u001b[m" + filme.get("image"));
+            System.out.println("\u001b[1mClassificação: " + filme.get("imDbRating") + "\u001b[m");
+            System.out.println(estrela + "\u001b[m");
+        }
+    }
+}
